31fc43979e8601cf6ce92bb46c764700332cc9d6
Sudeep Prasad <sudeep.rp@gmail.com>
2022-05-19 11:26:45 +0530

    switch to 2022

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ pushd "%~dp0"
 
 echo goingformsbuild
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" checker.csproj
+"%programfiles(x86)%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" checker.csproj
 
 popd
